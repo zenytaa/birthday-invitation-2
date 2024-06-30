e1ddba0bdf984710423abd5a951380c57f24fcbb
zen <alifanzennuranto@gmail.com>
2024-06-30 09:41:09 +0700

    feat: change vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+// import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  // plugins: [react()],
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  };
 });
